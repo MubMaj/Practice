9ac43a158e45b41e0a9743ba350dcb7b72a6f171
Mubashshir Majeed <131761327+MubMaj@users.noreply.github.com>
2024-02-12 16:21:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function leftBtn(){
+    
+}
+function rightBtn(){
+
+}
+
+const leftBtn = document.getElementById('left_btn')
+
+const leftText = document.getElementById('text-1').value
+
+
+leftBtn.addEventLisenter('click', function (){
+    
+
+})
