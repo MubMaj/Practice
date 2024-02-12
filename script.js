759a86fa57de6edfd0bab80ca69dcaf029d9d894
MubMaj <mub.maj@gmail.com>
2024-02-12 16:46:18 +0530

    Complete Code

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-function leftBtn(){
-    
-}
-function rightBtn(){
-
-}
-
 const leftBtn = document.getElementById('left_btn')
+const rightBtn = document.getElementById('right_btn')
+const clear_btn = document.getElementById('clear_btn')
 
-const leftText = document.getElementById('text-1').value
+
+let rightText = document.getElementById('text-2')
+let leftText = document.getElementById('text-1')
 
 
-leftBtn.addEventLisenter('click', function (){
-    
+leftBtn.addEventListener('click', function(){
+    leftText.value += '\n' + rightText.value;
+    rightText.value = '';
+})
 
+rightBtn.addEventListener('click', function (){
+    rightText.value += '\n' + leftText.value;
+    leftText.value = '';
+})
+
+clear_btn.addEventListener('click', function (){
+    leftText.value = '';
+    rightText.value = '';
 })
